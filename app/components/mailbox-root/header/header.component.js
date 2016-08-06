78484c085eb178d7f412e85805e9c127fbc22e88
arthur-kv <arthur@MacBook-Pro-Arthur.local>
2016-08-06 03:33:08 -0400

    feat: add header component

---
+++ app/components/mailbox-root/header/header.component.js
@@ -0,0 +1,9 @@
+'use strict';
+
+export default app => {
+
+	app.component('header', {
+		template: require('./header.html')
+	});
+
+}
--- app/components/mailbox-root/root.component.js
+++ app/components/mailbox-root/root.component.js
@@ -2,10 +2,14 @@
 
 import route from './root.route';
 
+import header from './header/header.component';
+
 export default app => {
+	/* desp */
+	header(app);
 
+	/* component */
 	require('./root.scss');
-
 	route(app);
 
 	app.component('mailboxRoot', {
