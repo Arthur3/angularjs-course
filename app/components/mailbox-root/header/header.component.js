54a381637bc8d6e6724f2a23849909c4e9524bba
arthur-kv <qw1r1@yandex.ru>
2016-08-06 07:58:35 -0400

    feat: connected searchbox component to header, moved header's and searchbox's styles to separate files

--- app/components/mailbox-root/header/header.component.js
+++ app/components/mailbox-root/header/header.component.js
@@ -1,6 +1,12 @@
 'use strict';
 
+import searchbox from './searchbox/searchbox.component';
+
 export default app => {
+	/* deps */
+	searchbox(app);
+
+	require('./header.scss');
 
 	app.component('mbHeader', {
 		template: require('./header.html')
