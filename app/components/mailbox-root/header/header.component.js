06eb348a0863ced8f0e355caf817a0f1225f3be4
arthur-kv <qw1r1@yandex.ru>
2016-08-06 08:09:49 -0400

    feat: connected "profile" component to header

--- app/components/mailbox-root/header/header.component.js
+++ app/components/mailbox-root/header/header.component.js
@@ -1,10 +1,12 @@
 'use strict';
 
 import searchbox from './searchbox/searchbox.component';
+import profile from './profile/profile.component';
 
 export default app => {
 	/* deps */
 	searchbox(app);
+	profile(app);
 
 	require('./header.scss');
 
