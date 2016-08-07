d8c3174fd60f788aaabec5a44aaceb70464caf23
arthur-kv <qw1r1@yandex.ru>
2016-08-07 02:41:41 -0400

    feat: created "sidebar" component

---
+++ app/components/mailbox-root/sidebar/sidebar.component.js
@@ -0,0 +1,21 @@
+'use strict';
+
+export default app => {
+
+	require('./sidebar.scss');
+
+	app.component('sidebar', {
+		template: require('./sidebar.html'),
+		bindings: {
+			items: '<'
+		},
+		controller: [function () {
+			this.itemClass = function () {
+				// TODO
+				// active, pending
+				return {};
+			}
+		}]
+	})
+
+}
