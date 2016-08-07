6c901045d478d9716ad1a375b5d2772488ba2645
arthur-kv <qw1r1@yandex.ru>
2016-08-07 02:51:16 -0400

    feat: load mailboxes list for sidemenu in index route's resolve

--- app/components/mailbox-root/root.route.js
+++ app/components/mailbox-root/root.route.js
@@ -4,7 +4,17 @@ export default app => {
 	app.config($stateProvider => {
 		$stateProvider.state('index', {
 			url: '/',
-			template: '<mailbox-root></mailbox-root>'
-		})
+			template: '<mailbox-root mailboxes="mailboxes"></mailbox-root>',
+			resolve: {
+				mailboxes: ['MailboxService', function (MailboxService) {
+					return MailboxService.loadMailBoxes().then(function (resp) {
+						return resp.data;
+					});
+				}]
+			},
+			controller: ['$scope', 'mailboxes', function ($scope, mailboxes) {
+				$scope.mailboxes = mailboxes;
+			}]
+		});
 	});
 }
