8c6e1d827d2334bc8194e9b26c2a850b1ed42ed9
arthur-kv <qw1r1@yandex.ru>
2016-08-07 02:51:46 -0400

    chore: linked sidebar component

--- app/components/mailbox-root/root.component.js
+++ app/components/mailbox-root/root.component.js
@@ -6,16 +6,27 @@ import header from './header/header.component';
 
 import controlbar from './controlbar/controlbar.component';
 
+import sidebar from './sidebar/sidebar.component';
+
+import mailboxService from './mailbox.service';
+
 export default app => {
 	/* deps */
 	header(app);
 	controlbar(app);
+	sidebar(app);
 
 	/* component */
 	require('./root.scss');
 	route(app);
+	mailboxService(app);
 
 	app.component('mailboxRoot', {
-		template: require('./root.html')
+		template: require('./root.html'),
+		bindings: {
+			mailboxes: '<'
+		},
+		controller: [function () {
+		}]
 	});
 }
