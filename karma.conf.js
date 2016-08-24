d2faab6571f85808d725e95d54858f2d229ead4f
arthur-kv <qw1r1@yandex.ru>
2016-08-24 09:08:01 -0400

    chore: installed "sinon"

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai'],
+    frameworks: ['mocha', 'chai', 'sinon'],
 
 
     // list of files / patterns to load in the browser
@@ -78,6 +78,7 @@ module.exports = function(config) {
         require('karma-webpack'),
         'karma-chai',
         'karma-mocha',
+        'karma-sinon',
         'karma-chrome-launcher'
     ]
   })
